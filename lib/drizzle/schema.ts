92570188d175d0d4d50214e6186b2c344579ddb5
Srinath10X <srinathcreates@gmail.com>
2025-07-01 22:28:03 +0530

    refactor: removed redundant drizzle configs and migrations

--- lib/drizzle/schema.ts
+++
@@ -1,7 +0,0 @@
-import { json, pgTable, text, uuid } from "drizzle-orm/pg-core";
-
-export const notes = pgTable("notes", {
-  id: uuid("id").primaryKey(),
-  title: text("title").notNull(),
-  content: json("content").notNull(),
-});
--- orm/0000_faulty_domino.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "notes" (
-	"id" uuid PRIMARY KEY NOT NULL,
-	"title" text NOT NULL,
-	"content" json NOT NULL
-);
