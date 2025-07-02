2a50e66732b566f4c2a78a0559fbe467f2459314
Srinath10X <srinathcreates@gmail.com>
2025-07-02 23:21:08 +0530

    feat(sidebar): made sidebar resizable and no overflow layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,7 @@
 export default function MainLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <div className="h-screen w-full flex">{children}</div>;
+  return (
+    <main className="h-screen w-full flex overflow-hidden">{children}</main>
+  );
 }
