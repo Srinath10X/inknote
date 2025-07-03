45034dff00f4e2838de08a0da11a5dfd5c799c67
Srinath10X <srinathcreates@gmail.com>
2025-07-03 11:59:15 +0530

    feat(api): added create and delete endpoints

--- app/(main)/api/notes/route.ts
+++ app/(main)/api/notes/route.ts
@@ -1,8 +1,8 @@
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { db } from "@/lib/drizzle/client";
 import { notes } from "@/lib/drizzle/schema";
 import { createClient } from "@/lib/supabase/server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 async function getAuthenticatedUser() {
   const supabase = await createClient();
@@ -16,10 +16,16 @@ async function getAuthenticatedUser() {
 export async function GET() {
   const user = await getAuthenticatedUser();
 
-  if (!user) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  if (!user) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
 
   const userId = user?.id;
-  const userNotes = await db.select().from(notes).where(eq(notes.userId, userId)).execute();
+  const userNotes = await db
+    .select()
+    .from(notes)
+    .where(eq(notes.userId, userId))
+    .execute();
 
   return NextResponse.json(userNotes);
 }
@@ -27,9 +33,35 @@ export async function GET() {
 export async function POST() {
   const user = await getAuthenticatedUser();
 
-  if (!user) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  if (!user) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
   const userId = user?.id;
 
   await db.insert(notes).values({ userId });
-  return NextResponse.json({ message: "Note created successfully" }, { status: 201 });
+  return NextResponse.json(
+    { message: "Note created successfully" },
+    { status: 201 },
+  );
+}
+
+export async function DELETE(request: NextRequest) {
+  const { noteId } = await request.json();
+  const user = await getAuthenticatedUser();
+
+  if (!user) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const userId = user?.id;
+
+  await db
+    .delete(notes)
+    .where(and(eq(notes.id, noteId), eq(notes.userId, userId)));
+
+  return NextResponse.json(
+    { message: "Note deleted successfully" },
+    { status: 200 },
+  );
 }
--- app/(main)/editor/page.tsx
+++ app/(main)/editor/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import ClientOnly from "@/components/ClientOnly";
 import { Editor } from "@/components/core/Editor";
 
 export default function DashboardPage() {
+  const [notes, setNotes] = useState<any[]>([]);
   const isResizing = useRef<boolean>(false);
 
   const editorRef = useRef<HTMLDivElement | null>(null);
@@ -42,13 +43,58 @@ export default function DashboardPage() {
     sidebarRef.current!.style.width = `${width}px`;
   };
 
+  async function getNotes() {
+    const res = await fetch("/api/notes");
+    const data = await res.json();
+
+    setNotes(data);
+  }
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  async function createNote() {
+    await fetch("/api/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    getNotes();
+  }
+
+  async function deleteNotes(id: string) {
+    await fetch("/api/notes", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ noteId: id }),
+    });
+    getNotes();
+  }
+
   return (
     <>
       <aside
         ref={sidebarRef}
         className="w-64 flex justify-between group/sidebar"
       >
-        Another div
+        <ul className="flex flex-col">
+          {notes.map((note, id) => (
+            <button key={id} onClick={async () => await deleteNotes(note.id)}>
+              {note.noteCtx.note_title}
+            </button>
+          ))}
+          <button
+            className="p-2 bg-[#1e1e1e] text-white rounded-md"
+            onClick={createNote}
+          >
+            Create
+          </button>
+        </ul>
         <div
           className="h-full w-1 cursor-ew-resize group-hover/sidebar:bg-gray-300"
           onMouseDown={onMouseDown}
