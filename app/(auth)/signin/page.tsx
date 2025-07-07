07d657b0e74d2fbac98c5b28d61da4786d367d90
Srinath10X <srinathcreates@gmail.com>
2025-07-07 11:36:38 +0530

    ui: created and updated shadcn login component

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -2,6 +2,10 @@
 
 import { useState } from "react";
 import { supabase } from "@/lib/supabase/client";
+import { GalleryVerticalEnd } from "lucide-react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function SignInPage() {
   const [email, setEmail] = useState("");
@@ -36,37 +40,91 @@ export default function SignInPage() {
   };
 
   return (
-    <div className="p-4 flex flex-col gap-4">
-      <h1 className="text-xl font-bold">Log In</h1>
-
-      {errorMsg && <p className="text-red-500">{errorMsg}</p>}
-
-      <form onSubmit={handleEmailLogin} className="flex flex-col gap-2">
-        <input
-          className="border p-2"
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          className="border p-2"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="bg-black text-white py-2 px-4 rounded" type="submit">
-          Log In with Email
-        </button>
-      </form>
-
-      <button
-        onClick={handleOAuthLogin}
-        className="bg-blue-500 text-white py-2 px-4 rounded"
-      >
-        Log In with Google
-      </button>
+    <div className="grid min-h-svh">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Inknote
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <form className="flex flex-col gap-6">
+              <div className="flex flex-col items-center gap-2 text-center">
+                <h1 className="text-2xl font-bold">Login to your account</h1>
+                <p className="text-muted-foreground text-sm text-balance">
+                  Enter your email below to login to your account
+                </p>
+              </div>
+              <div className="grid gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a
+                      href="#"
+                      className="ml-auto text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input
+                    id="password"
+                    type="password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <Button
+                  type="submit"
+                  className="w-full active:scale-95 hover:cursor-pointer"
+                  onClick={handleEmailLogin}
+                >
+                  Login
+                </Button>
+                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                  <span className="bg-background text-muted-foreground relative z-10 px-2">
+                    Or continue with
+                  </span>
+                </div>
+                <Button
+                  variant="outline"
+                  className="w-full active:scale-95 hover:cursor-pointer"
+                  onClick={handleOAuthLogin}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path
+                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                  Continue with Google
+                </Button>
+              </div>
+              <div className="text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <a href="#" className="underline underline-offset-4">
+                  Sign up
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
