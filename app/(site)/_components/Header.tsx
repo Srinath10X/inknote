30111b7f45d90a748c4f5cfa1628d2903459de1b
Srinath10X <srinathcreates@gmail.com>
2025-10-30 22:49:05 +0530

    feat: implemented fluid design

---
+++ app/(site)/_components/Header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Inknote</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Work</a>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,22 @@
+import Header from "./_components/Header";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello</h1>
+      <div className="mx-auto px-4 sm:px-6 lg:px-0 max-w-md sm:max-w-160 lg:max-w-240 xl:max-w-304">
+        <Header />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quas
+        inventore maxime mollitia qui cum enim neque voluptatibus soluta
+        aperiam, porro cumque fugit a nam amet quam sequi vero eveniet quisquam
+        nihil tenetur molestias aliquam voluptas. Laborum hic vel consequatur
+        quidem veniam aut culpa est amet quasi mollitia, architecto assumenda
+        voluptates rerum nam? Nulla fugiat delectus praesentium et aspernatur
+        totam ad odio accusantium dolorum. Et repellat dolor asperiores vero
+        eaque aliquam? Cupiditate, aliquam blanditiis. Illo recusandae deleniti
+        alias autem iusto veritatis dolores ipsa voluptate eveniet architecto id
+        accusamus sit corporis repellat aliquam labore, vitae est odit nulla,
+        tempora, consequuntur neque?
+      </div>
     </>
   );
 }
