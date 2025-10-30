be1522870f22ca492b8179e175270bf912c0f9af
Srinath10X <srinathcreates@gmail.com>
2025-10-31 00:21:44 +0530

    refactor(component): created `Link` component for nav links

--- app/(site)/_components/Header.tsx
+++ app/(site)/_components/Header.tsx
@@ -1,28 +1,58 @@
 import { Button } from "@/components/ui/button";
 
+/* TODO: Fix this scrap */
+function Link({
+  children,
+  href,
+}: {
+  children: React.ReactNode;
+  href?: string;
+}) {
+  return (
+    <a className="text-black/80 hover:text-black transition" href={href || "#"}>
+      {children}
+    </a>
+  );
+}
+
 export default function Header() {
+  const links = [
+    {
+      id: 1,
+      name: "Home",
+      href: "#",
+    },
+    {
+      id: 2,
+      name: "Features",
+      href: "#",
+    },
+    {
+      id: 3,
+      name: "Pricing",
+      href: "#",
+    },
+    {
+      id: 4,
+      name: "Blog",
+      href: "#",
+    },
+  ];
+
   return (
     <header className="sticky top-2 z-50">
       <div className="h-16 px-4 sm:px-6 flex items-center justify-between border border-black/10 rounded-xl bg-white/70 backdrop-blur-xl supports-backdrop-filter:bg-white/40">
         <h1 className="text-lg font-semibold tracking-tight">Inknote</h1>
 
-        {/* TODO: Fix this non-sense make a resuable component */}
         <nav className="hidden lg:flex items-center gap-6 text-sm font-medium">
-          <a className="text-black/80 hover:text-black transition" href="#">
-            Home
-          </a>
-          <a className="text-black/80 hover:text-black transition" href="#">
-            Features
-          </a>
-          <a className="text-black/80 hover:text-black transition" href="#">
-            Pricing
-          </a>
-          <a className="text-black/80 hover:text-black transition" href="#">
-            Blog
-          </a>
+          {links.map(({ id, name, href }) => (
+            <Link key={id} href={href}>
+              {name}
+            </Link>
+          ))}
         </nav>
 
-        <div className="flex items-center">
+        <div className="flex items-center gap-2">
           <Button variant={"ghost"}>Login</Button>
           <Button variant={"default"}>Signup</Button>
         </div>
