d87e5c6d557ac320ba994d192323b76ca9c4b235
Srinath10X <srinathcreates@gmail.com>
2025-10-30 23:56:41 +0530

    feat: built responsive header

--- app/(site)/_components/Header.tsx
+++ app/(site)/_components/Header.tsx
@@ -1,17 +1,32 @@
+import { Button } from "@/components/ui/button";
+
 export default function Header() {
   return (
-    <header>
-      <h1>Inknote</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Work</a>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </nav>
+    <header className="sticky top-2 z-50">
+      <div className="h-16 px-4 sm:px-6 flex items-center justify-between border border-black/10 rounded-xl bg-white/70 backdrop-blur-xl supports-backdrop-filter:bg-white/40">
+        <h1 className="text-lg font-semibold tracking-tight">Inknote</h1>
+
+        {/* TODO: Fix this non-sense make a resuable component */}
+        <nav className="hidden lg:flex items-center gap-6 text-sm font-medium">
+          <a className="text-black/80 hover:text-black transition" href="#">
+            Home
+          </a>
+          <a className="text-black/80 hover:text-black transition" href="#">
+            Features
+          </a>
+          <a className="text-black/80 hover:text-black transition" href="#">
+            Pricing
+          </a>
+          <a className="text-black/80 hover:text-black transition" href="#">
+            Blog
+          </a>
+        </nav>
+
+        <div className="flex items-center">
+          <Button variant={"ghost"}>Login</Button>
+          <Button variant={"default"}>Signup</Button>
+        </div>
+      </div>
     </header>
   );
 }
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,19 +3,68 @@ import Header from "./_components/Header";
 export default function Home() {
   return (
     <>
-      <div className="mx-auto px-4 sm:px-6 lg:px-0 max-w-md sm:max-w-160 lg:max-w-240 xl:max-w-304">
+      <div className="mx-auto px-4 sm:px-6 md:px-8 lg:px-0 lg:max-w-240 xl:max-w-304">
         <Header />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quas
-        inventore maxime mollitia qui cum enim neque voluptatibus soluta
-        aperiam, porro cumque fugit a nam amet quam sequi vero eveniet quisquam
-        nihil tenetur molestias aliquam voluptas. Laborum hic vel consequatur
-        quidem veniam aut culpa est amet quasi mollitia, architecto assumenda
-        voluptates rerum nam? Nulla fugiat delectus praesentium et aspernatur
-        totam ad odio accusantium dolorum. Et repellat dolor asperiores vero
-        eaque aliquam? Cupiditate, aliquam blanditiis. Illo recusandae deleniti
-        alias autem iusto veritatis dolores ipsa voluptate eveniet architecto id
-        accusamus sit corporis repellat aliquam labore, vitae est odit nulla,
-        tempora, consequuntur neque?
+        <h2 className="text-5xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
+          labore consequatur repellendus eius fuga vero reiciendis
+          necessitatibus distinctio modi vel nesciunt ab nam molestias quam
+          ratione optio nisi ducimus natus aliquid commodi, expedita officia.
+          Tempore id, libero ducimus ipsa aspernatur animi illum voluptate totam
+          temporibus vel impedit quasi? Sapiente modi magnam aperiam! Doloremque
+          praesentium animi aliquam consequuntur distinctio rerum expedita id
+          similique ducimus, sunt error ipsa laudantium nam tempora accusamus.
+          Doloribus quaerat ipsum facere debitis praesentium quia ratione
+          cupiditate animi deleniti assumenda libero commodi numquam recusandae
+          vero sit porro, saepe cumque fugit ducimus ipsa dolor itaque facilis
+          officia. At reiciendis cupiditate totam, adipisci accusantium
+          molestias ad tempore voluptatem? Vel minus eius molestiae architecto
+          voluptate quos blanditiis ipsam quasi at reprehenderit enim totam
+          dolorum, perferendis eveniet error? At fuga vel, eveniet optio harum
+          sapiente veritatis, repudiandae excepturi, minus dolores vitae
+          consequuntur quibusdam dignissimos ullam enim veniam quos mollitia
+          blanditiis facere iure eaque voluptatum! Rem itaque totam natus sequi
+          perspiciatis? Similique veniam laborum eaque nemo! Quas a minus ea
+          atque ipsam corporis eum, veritatis sit ratione ipsum maiores
+          blanditiis, id eligendi voluptatem harum nemo quasi vel distinctio
+          voluptates adipisci animi fuga aliquid saepe? Amet consequatur sit vel
+          impedit. Perferendis iusto at voluptatem aut? Maxime, animi, saepe
+          facilis nulla explicabo beatae delectus repudiandae quibusdam sint
+          natus quisquam similique. Est exercitationem ex obcaecati eligendi
+          dolore dolores porro corporis nostrum amet, similique omnis ullam.
+          Aliquid ducimus obcaecati sint ullam temporibus consequuntur assumenda
+          doloribus architecto, porro voluptas vel at dolorum omnis dignissimos
+          repellat laborum velit magnam iste voluptates unde. Quisquam laborum
+          officiis vitae omnis delectus, assumenda, fuga rem error
+          necessitatibus suscipit nulla perspiciatis ipsum repellendus similique
+          aperiam quidem architecto tenetur at ad accusantium dignissimos. Dolor
+          sequi blanditiis, recusandae a temporibus ab exercitationem, deleniti
+          ipsum fugiat sunt unde eligendi at fuga reprehenderit eos, explicabo
+          nostrum earum ut! Accusamus quidem odio nulla deleniti vitae nisi
+          fugiat reiciendis expedita, distinctio veniam voluptates perspiciatis
+          possimus recusandae, inventore placeat, sed consectetur provident?
+          Tenetur eveniet ut magni eligendi sequi molestias reiciendis
+          consequatur doloremque nihil? Optio libero nisi explicabo, veritatis
+          numquam repellat modi, culpa totam aut accusantium ad velit minima
+          dolorum sequi non. Aliquid, maxime debitis voluptate optio incidunt
+          quibusdam velit modi tempora enim. Voluptate eveniet exercitationem
+          voluptas rerum in accusantium labore, excepturi mollitia ducimus
+          consequuntur sapiente aut sed fugiat dolorem esse ratione totam ipsa
+          doloribus. Dolore ratione doloribus consequuntur dolorum mollitia
+          placeat, perspiciatis obcaecati? Cum officiis nulla consequatur
+          quidem, corporis nam voluptates distinctio, corrupti nisi et error
+          optio a aliquam commodi laboriosam eveniet minus velit nobis
+          cupiditate esse inventore natus. Illum dolore vel, corporis sed
+          maiores, dolores magni in atque sequi reprehenderit unde ducimus cum
+          aperiam. Culpa vitae libero voluptates perferendis itaque aperiam, eos
+          quae cumque voluptatem. Similique ad voluptatibus, totam, minima, sed
+          sapiente fugit eaque est unde quisquam qui error nihil asperiores.
+          Cumque asperiores voluptas ullam tempore ad quod neque nulla delectus
+          maxime alias accusantium accusamus veniam dolor amet dolores
+          reprehenderit, vero at animi possimus illum minima commodi tenetur
+          earum. Cupiditate perferendis sed, expedita ratione hic voluptatem
+          laborum et numquam omnis.
+        </h2>
       </div>
     </>
   );
