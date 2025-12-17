4065c18e0e042f456b020faf537fdd7ecd31c626
Srinath10X <srinathcreates@gmail.com>
2025-12-17 22:18:14 +0530

    feat: created signin page

---
+++ src/components/login-form.tsx
@@ -0,0 +1,86 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+	Field,
+	FieldDescription,
+	FieldGroup,
+	FieldLabel,
+	FieldSeparator,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+export function LoginForm({
+	className,
+	...props
+}: React.ComponentProps<"form">) {
+	return (
+		<form className={cn("flex flex-col gap-6", className)} {...props}>
+			<FieldGroup>
+				<div className="flex flex-col items-center gap-1 text-center">
+					<h1 className="text-2xl font-bold">Login to your account</h1>
+					<p className="text-muted-foreground text-sm text-balance">
+						Enter your email below to login to your account
+					</p>
+				</div>
+				<Field>
+					<FieldLabel htmlFor="email">Email</FieldLabel>
+					<Input id="email" type="email" placeholder="m@example.com" required />
+				</Field>
+				<Field>
+					<div className="flex items-center">
+						<FieldLabel htmlFor="password">Password</FieldLabel>
+						<a
+							href="#"
+							className="ml-auto text-sm underline-offset-4 hover:underline"
+						>
+							Forgot your password?
+						</a>
+					</div>
+					<Input id="password" type="password" required />
+				</Field>
+				<Field>
+					<Button type="submit">Login</Button>
+				</Field>
+				<FieldSeparator>Or continue with</FieldSeparator>
+				<Field>
+					<Button variant="outline" type="button">
+						<svg viewBox="0 0 48 48" className="h-4 w-4">
+							<path
+								fill="#EA4335"
+								d="M24 9.5c3.54 0 6.06 1.54 7.45 2.83l5.45-5.45C33.64 3.89 29.21 2 24 2 14.82 2 7.1 7.94 4.24 16.26l6.68 5.18C12.59 15.36 17.88 9.5 24 9.5z"
+							/>
+							<path
+								fill="#4285F4"
+								d="M46.5 24c0-1.64-.15-3.22-.43-4.74H24v9.02h12.7c-.55 2.98-2.22 5.5-4.72 7.2l7.23 5.6C43.98 36.3 46.5 30.68 46.5 24z"
+							/>
+							<path
+								fill="#FBBC05"
+								d="M10.92 28.44A14.5 14.5 0 0 1 10.1 24c0-1.54.27-3.02.82-4.44l-6.68-5.18A23.99 23.99 0 0 0 2 24c0 3.92.94 7.63 2.24 10.62l6.68-5.18z"
+							/>
+							<path
+								fill="#34A853"
+								d="M24 46c6.21 0 11.43-2.05 15.24-5.58l-7.23-5.6c-2.01 1.35-4.58 2.14-8.01 2.14-6.12 0-11.41-5.86-13.08-11.94l-6.68 5.18C7.1 40.06 14.82 46 24 46z"
+							/>
+						</svg>
+						Continue with Google
+					</Button>
+					<Button variant="outline" type="button">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+							<path
+								d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+								fill="currentColor"
+							/>
+						</svg>
+						Continue with GitHub
+					</Button>
+					<FieldDescription className="text-center">
+						Don&apos;t have an account?{" "}
+						<a href="#" className="underline underline-offset-4">
+							Sign up
+						</a>
+					</FieldDescription>
+				</Field>
+			</FieldGroup>
+		</form>
+	)
+}
