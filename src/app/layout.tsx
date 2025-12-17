2308c88149afc78ad142af6fd77692df69890179
Srinath10X <srinathcreates@gmail.com>
2025-12-17 20:51:00 +0530

    rewrite: init

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "./global.css";
+import type { Metadata } from "next";
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Inknote",
+	description: "A minimal yet powerfull note taking application",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{ children: React.ReactNode }>) {
+	return (
+		<html lang="en">
+			<body className={`${inter.className} antialiased`}>{children}</body>
+		</html>
+	);
+}
